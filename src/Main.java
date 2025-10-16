74617f9da2bf7d20062b4ed1e8fc3b2747c92abb
ICH <chumacheva@gmail.com>
2025-10-16 17:03:11 +0300

    Изменена логика программы на вычислительную

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>");
-        String text=new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите 1e число");
+        int first_number = new Scanner(System.in).nextInt();
+        //System.out.println(first_number);
+        System.out.println("Введите 2e число");
+        int second_number = new Scanner(System.in).nextInt();
+       // System.out.println(second_number);
+        int sum=first_number+second_number;
+        System.out.println("Сумма = "+sum);
+        int dif=first_number-second_number;
+        System.out.println("Разность = "+ dif);
+        int op=first_number*second_number;
+        System.out.println("Произведение = " +op);
+
+        if (second_number == 0) {
+            System.out.println("Частное = Введенное вами второе число=0, делить на ноль нельзя");
+        }
+        else {
+            double quotient = (double) first_number / second_number;
+            System.out.println("Частное = " + quotient);
+        }
     }
 }
